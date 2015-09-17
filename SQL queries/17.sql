fa2a847ffd559f7a2dda7c51a655a55a536838bd
Lando B <jrolandobernal@gmail.com>
2015-09-17 12:36:41 -0500

    Completed 11 to 17

--- SQL queries/17.sql
+++ SQL queries/17.sql
@@ -1,1 +1,3 @@
-SELECT BillingCountry FROM Invoice GROUP BY BillingCountry
+SELECT Invoice.*, COUNT(*) AS "NumberOfLineItems" FROM Invoice
+INNER JOIN InvoiceLine ON Invoice.InvoiceId == InvoiceLine.InvoiceId
+GROUP BY Invoice.InvoiceId ORDER BY Invoice.InvoiceId
