f651d9cd4cc2c5561de9799a4b4cd0971639a4ef
Lando B <jrolandobernal@gmail.com>
2015-09-17 12:44:07 -0500

    Minor chage to 18 to format the Total Sales a little bit better

--- SQL queries/18.sql
+++ SQL queries/18.sql
@@ -1,4 +1,4 @@
-SELECT Employee.FirstName, Employee.LastName, PRINTF(SUM(Invoice.Total)) AS "Total Sales" 
+SELECT Employee.FirstName, Employee.LastName, PRINTF("$%.2f", SUM(Invoice.Total)) AS "Total Sales" 
 FROM Invoice
 INNER JOIN Customer ON Invoice.CustomerId == Customer.CustomerId
 INNER JOIN Employee ON Customer.SupportRepId == Employee.EmployeeId
